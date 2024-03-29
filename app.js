81d11b7aee05cffe2f91606b937708b28e0ec9e2
Abdul Moiz Sabro <bravia.mj@gmail.com>
2024-03-30 03:52:16 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// document.write("HI")
+
+function boxa(){
+    document.getElementById('abc').innerHTML = 'Box 1'
+    document.getElementById('abc').value = ''
+}
+function boxb(){
+    document.getElementById('boxx2').innerHTML = 'Box 2'
+}
+function boxc(){
+    document.getElementById('boxx3').innerHTML = 'Box 3'
+}
+function boxd(){
+    document.getElementById('boxx4').innerHTML = 'Box 4'
+}
+function boxe(){
+    document.getElementById('boxx5').innerHTML = 'Box 5'
+}
+function boxf(){
+    document.getElementById('boxx6').innerHTML = 'Box 6'
+}
+function boxg(){
+    document.getElementById('boxx7').innerHTML = 'Box 7'
+}
+function boxh(){
+    document.getElementById('boxx8').innerHTML = 'Box 8'
+}
